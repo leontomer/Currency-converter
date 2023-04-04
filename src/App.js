722fd52a3f906930eb6c9afadf5b0c0c3f5709d9
leontomer <41841316+leontomer@users.noreply.github.com>
2023-04-04 15:44:41 +0300

    fixed some naming of dir and variable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Converter from "./components/converter/Converter";
+import Converter from "./components/Converter/converter";
 function App() {
   return (
     <div className="app">
